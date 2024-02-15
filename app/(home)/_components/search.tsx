30836ff7c9a3138220bc41879b7176dfe655c9f2
Adriel Itamar <adrielitamar@gmail.com>
2024-02-15 15:02:56 -0300

    feat: add search component

---
+++ app/(home)/_components/search.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/app/_components/ui/button";
+import { Input } from "@/app/_components/ui/input";
+import { SearchIcon } from "lucide-react";
+
+const Search = () => {
+    return ( 
+        <div className="items-center flex gap-2">
+            <Input placeholder="Busque uma barbearia"/>
+            <Button variant="default" size="icon">
+                <SearchIcon size={18} />
+            </Button>
+        </div>
+     );
+}
+ 
+export default Search;
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,18 +1,23 @@
 import { format } from "date-fns"
 import Header from "../_components/header";
 import { ptBR } from "date-fns/locale";
+import Search from "./_components/search";
 
 export default function Home() {
   return (
     <div>
       <Header />
 
-      <div className="px-5 pt-5">
+      <section className="px-5 pt-5">
         <h2 className="text-xl font-bold">Olá, Adriel</h2>
         <p className="capitalize text-sm">
           {format(new Date(), "EEEE',' dd 'de' MMMM", {
             locale: ptBR
           })}</p>
+      </section>
+
+      <div className="px-5 mt-6">
+        <Search />
       </div>
 
     </div>
