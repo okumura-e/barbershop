e7f55a8f3b0e6f83159e2b99389de13a7418acb9
Adriel Itamar <adrielitamar@gmail.com>
2025-03-15 11:53:08 -0300

    feat: add configs

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,17 +1,6 @@
-import { db } from "@/app/_lib/prisma";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import NextAuth from "next-auth";
-import { Adapter } from "next-auth/adapters";
-import GoogleProvider from "next-auth/providers/google";
+import { authOptions } from '@/app/_lib/auth';
+import NextAuth from 'next-auth';
 
-const handler = NextAuth({
-  adapter: PrismaAdapter(db) as Adapter,
-  providers: [
-    GoogleProvider({
-      clientId: "",
-      clientSecret: "",
-    }),
-  ],
-});
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
