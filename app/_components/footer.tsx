d1b110dbe4495f65eae720c4c2553ec10b364871
Adriel Itamar <adrielitamar@gmail.com>
2025-03-15 11:52:13 -0300

    feat: add components

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,9 +1,10 @@
 const Footer = () => {
-    return ( 
-        <section className="w-full bg-secondary py-6 px-5">
-            <p className="text-gray-400 text-xs font-bold opacity-75">© 2024 Copyright FSW Barber</p>
-        </section>
-     );
-}
- 
-export default Footer;
+    return (
+      <footer className="w-full bg-secondary py-6 px-5">
+        <p className="text-gray-400 text-xs opacity-75">
+          © 2025 Copyright <b>FSW Barber</b>
+        </p>
+      </footer>
+    );
+  };
+  export default Footer;
--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,19 +1,42 @@
-import Image from "next/image";
-import { Card, CardContent } from "./ui/card";
-import { Button } from "./ui/button";
-import { MenuIcon } from "lucide-react";
+'use client';
+
+import Image from 'next/image';
+import { Card, CardContent } from './ui/card';
+import { Button } from './ui/button';
+import { MenuIcon } from 'lucide-react';
+import { Sheet, SheetContent, SheetTrigger } from './ui/sheet';
+import SideMenu from './side-menu';
+import { redirect } from 'next/navigation';
+import Link from 'next/link';
 
 const Header = () => {
-    return ( 
-        <Card>
-            <CardContent className="p-5 flex items-center justify-between">
-                <Image src="/logo.svg" alt="barber logo" height={22} width={120}/>
-                <Button variant="outline" size="icon" className="h-8 w-8">
-                    <MenuIcon size={18} />
-                </Button>
-            </CardContent>
-        </Card>
-     );
-}
- 
+  return (
+    <header>
+      <Card className="rounded-none">
+        <CardContent className="p-5 justify-between items-center flex flex-row">
+          <Link href="/">
+            <Image
+              src="/logo-cheia.png"
+              alt="FSW Barber"
+              height={18}
+              width={120}
+              className="w-auto h-auto"
+            />
+          </Link>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="outline" size="icon">
+                <MenuIcon size={18} />
+              </Button>
+            </SheetTrigger>
+
+            <SheetContent className="p-0">
+              <SideMenu />
+            </SheetContent>
+          </Sheet>
+        </CardContent>
+      </Card>
+    </header>
+  );
+};
 export default Header;
